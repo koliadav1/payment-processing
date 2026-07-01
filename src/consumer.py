f427dbc16ec0dd4f70d9dd26b7fda6c0dd4691b7
Nikolay Davydov <koliadav123@gmail.com>
2026-07-01 16:14:56 +0700

    added dlq and logger

--- src/consumer.py
+++ src/consumer.py
@@ -1,7 +1,6 @@
 import asyncio
 import random
-
-from faststream import FastStream
+from faststream import FastStream, Logger
 from faststream.rabbit import (
     ExchangeType,
     RabbitBroker,
@@ -23,12 +22,21 @@ app = FastStream(broker)
 main_queue = RabbitQueue(MAIN_QUEUE_NAME, durable=True)
 
 RETRY_DELAYS_MS = [2000, 4000, 8000]
-RETRY_EXCHANGE = "x-payments-retries"
+RETRY_EXCHANGE_NAME = "x-payments-retries"
+DLQ_TTL = 1000 * 60 * 60 * 24 * 7  # 1 неделя
+DLQ_NAME = "payments.dlq"
+DLQ_EXCHANGE_NAME = "x-payments-dlq"
 
 retry_queues = []
-
 retry_exchange = RabbitExchange(
-    name=RETRY_EXCHANGE, type=ExchangeType.DIRECT, durable=True
+    name=RETRY_EXCHANGE_NAME, type=ExchangeType.DIRECT, durable=True
+)
+
+dead_queue = RabbitQueue(
+    DLQ_NAME, durable=True, arguments={"x-message-ttl": DLQ_TTL}
+)
+dlq_exchange = RabbitExchange(
+    name=DLQ_EXCHANGE_NAME, type=ExchangeType.DIRECT, durable=True
 )
 
 for i, delay in enumerate(RETRY_DELAYS_MS, start=1):
@@ -37,7 +45,7 @@ for i, delay in enumerate(RETRY_DELAYS_MS, start=1):
         durable=True,
         arguments={
             "x-message-ttl": delay,
-            "x-dead-letter-exchange": RETRY_EXCHANGE,
+            "x-dead-letter-exchange": RETRY_EXCHANGE_NAME,
             "x-dead-letter-routing-key": MAIN_QUEUE_NAME,
         },
     )
@@ -50,12 +58,18 @@ async def setup():
     q = await broker.declare_queue(main_queue)
     await q.bind(x, routing_key=MAIN_QUEUE_NAME)
 
+    dlq_x = await broker.declare_exchange(dlq_exchange)
+    dlq = await broker.declare_queue(dead_queue)
+    await dlq.bind(dlq_x, routing_key=DLQ_NAME)
+
     for rq in retry_queues:
         await broker.declare_queue(rq)
 
 
 @broker.subscriber(main_queue)
-async def handle_payment_event(event: PaymentEventPayload, msg: RabbitMessage):
+async def handle_payment_event(
+    event: PaymentEventPayload, msg: RabbitMessage, logger: Logger
+):
     headers = msg.headers or {}
     curr_retry = int(headers.get("x-retry-count", 0))
 
@@ -72,8 +86,9 @@ async def handle_payment_event(event: PaymentEventPayload, msg: RabbitMessage):
         await asyncio.sleep(processing_time)
 
         chance = random.random()
-        if chance < 0.8:
+        if chance < 0.1:
             if curr_retry < len(RETRY_DELAYS_MS):
+                logger.warning(f"Попытка {curr_retry} провалилась")
                 next_retry = curr_retry + 1
                 target_queue = f"payments.retry.{next_retry}"
                 headers["x-retry-count"] = next_retry
@@ -82,10 +97,12 @@ async def handle_payment_event(event: PaymentEventPayload, msg: RabbitMessage):
                     message=event, queue=target_queue, headers=headers
                 )
             else:
-                # TODO dql
-                raise Exception("Temp exception")
+                logger.error(f"Платеж {event.id} не обработан")
+                payment.status = PaymentStatus.FAILED
+                await uow.payments_repo.update(payment)
+                await broker.publish(message=event, queue=dead_queue)
             return
-
+        logger.info(f"Платеж {event.id} обработан успешно")
         payment.status = PaymentStatus.SUCCEEDED
         await uow.payments_repo.update(payment)
 
@@ -102,6 +119,6 @@ async def handle_payment_event(event: PaymentEventPayload, msg: RabbitMessage):
                 url=str(payment.webhook_url), json=webhook_data, timeout=5
             )
     except Exception as e:
-        print(
+        logger.error(
             f"Не удалось отправить уведомление на webhook {payment.webhook_url}, ошибка: {e}"
         )
